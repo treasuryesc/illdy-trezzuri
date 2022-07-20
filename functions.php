fa99a16892095f40e71628f910492b9bb503299c
Marco Fonseca <marco.aurelio@trezzuri.com.br>
2022-07-20 10:57:20 +0000

    fix image size and translations

--- functions.php
+++ functions.php
@@ -1,10 +1,13 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', function () {
-    wp_enqueue_style( 'illdy', get_template_directory_uri() . '/style.css' ); 
-} );
+add_action( 'wp_enqueue_scripts', 'child_enqueue_style' );
+add_action( 'after_setup_theme', 'child_after_setup_theme' ); 
 
-add_action( 'after_setup_theme', function () {
+function child_enqueue_style () {
+    wp_enqueue_style( 'illdy-trezzuri', get_template_directory_uri() . '/style.css' ); 
+} 
+
+function child_after_setup_theme () {
     load_child_theme_textdomain( 'illdy', get_stylesheet_directory() . '/languages' );
-} );
-
+    add_image_size( 'illdy-blog-list', 500, 500, true );
+} 
